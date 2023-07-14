eea28a45f59ea36769e58b6133ca8296c1a3b942
Tomas Valenta <valenta.and.thomas@gmail.com>
2023-07-14 00:27:28 -0600

    Fix task step types

--- agent/python/e2b_agent/agent.py
+++ agent/python/e2b_agent/agent.py
@@ -1,5 +1,7 @@
 import uvicorn
 
+from fastapi import FastAPI, Response
+from fastapi.responses import JSONResponse
 from typing import Awaitable, Callable, List, Optional
 from pydantic import BaseModel
 
@@ -17,17 +19,19 @@ AgentStepHandler = Callable[[AgentTask], Awaitable[AgentStep]]
 class Agent:
     @staticmethod
     def handle_task_step(handler: AgentStepHandler):
-        async def handler_wrapper(task_id: str) -> AgentStepResult:
+        async def handler_wrapper(task_id: str):
           task = next(filter(lambda t: t.task_id == task_id, tasks), None)
           if not task:
             raise Exception(f"Task with id {task_id} not found")
           
           result = await handler(task)
-          return AgentStepResult(
-             task_id=task_id,
-             input=task.input,
-             deliverables=result.deliverables,
-             output=result.output,
+          return JSONResponse(
+            AgentStepResult(
+              task_id=task_id,
+              input=task.input,
+              deliverables=result.deliverables,
+              output=result.output,
+            ).dict()
           )
 
         app.add_api_route(
